91250d23712aac182056df660a62793fe8528ba1
nasta <nasta@foxmail.com>
2013-08-27 16:22:27 +0800

    fix auto_now problem

--- ical/admin.py
+++ ical/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from ical.models import Calendar, Log
 
-admin.site.register(Calendar)
-admin.site.register(Log)
+class LogAdmin(admin.ModelAdmin):
+	fields = ['action', 'result', 'detail', 'date_time']
+	list_display = ('date_time', 'action', 'result')
+	list_filter = ['date_time', 'action']
+
+class CalendarAdmin(admin.ModelAdmin):
+	list_display = ('url', 'create_date')
+	list_filter = ['create_date']
+
+admin.site.register(Calendar, CalendarAdmin)
+admin.site.register(Log, LogAdmin)
--- ical/models.py
+++ ical/models.py
@@ -1,29 +1,42 @@
 from django.db import models
-
-class Login(models.Model):
-	username = models.CharField(max_length="12", primary_key=True)
-	password = models.CharField(max_length="20")
-
-class Calendar(models.Model):
-	url = models.CharField(max_length=6, primary_key=True)
-	class_table = models.TextField()
-	content = models.TextField()
-	create_date = models.DateTimeField("date created", auto_now=True)
-
-	@classmethod
-	def create(self, content=""):
-		import random, string
-		url = ''.join(random.sample(string.ascii_letters+string.digits, 8))
-		return Calendar(url=url, content=content)
-
-	def __unicode__(self):
-		return self.url
+import datetime
 
 class Log(models.Model):
 	action = models.CharField(max_length=20)
 	result = models.CharField(max_length=20)
 	detail = models.TextField()
-	date_time = models.DateTimeField("Date", auto_now=True)
+	date_time = models.DateTimeField("date_time")
 
 	def __unicode__(self):
 		return "%s : %s" % (self.action, self.result)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.date_time = datetime.datetime.now()
+		return super(Log, self).save(*args, **kwargs)
+
+class Login(models.Model):
+	username = models.CharField(max_length="12", primary_key=True)
+	password = models.CharField(max_length="20")
+
+
+class Calendar(models.Model):
+	url = models.CharField(max_length=6, primary_key=True)
+	class_table = models.TextField()
+	content = models.TextField()
+	create_date = models.DateTimeField("date created")
+
+	@classmethod
+
+	def create(self, content=""):
+		import random, string
+		url = ''.join(random.sample(string.ascii_letters+string.digits, 8))
+		return Calendar(url=url, content=content)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.date_time = datetime.datetime.now()
+		return super(Calendar, self).save(*args, **kwargs)
+
+	def __unicode__(self):
+		return self.url
