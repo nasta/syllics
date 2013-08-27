6e0c81f9f51c4b623b6151114735f1b089669549
nasta <nasta@foxmail.com>
2013-08-27 16:27:03 +0800

    some modification

--- ical/views.py
+++ ical/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from ical.models import Calendar, Login
 
-from mytjut import getClassTable
-from syllabus import getClassList
+from syllabus import getClassList, getClassTable
 
 def index(request):
     return render(request, "index.html")
