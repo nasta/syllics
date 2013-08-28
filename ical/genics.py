2eecd81f5a82ec0ebddd2f8ab4aab5eb97d0e24c
nasta <nasta@foxmail.com>
2013-08-28 16:27:23 +0800

    add comments

--- ical/genics.py
+++ ical/genics.py
@@ -6,6 +6,13 @@ import icalendar
 import pytz
 from datetime import datetime, timedelta
 
+################################################
+#
+#   上课时间表
+#   每一行是一节课
+#   第一个元组是开始的时间，第二个元组是结束时间
+#
+################################################
 timeTable = (
     None,
     ((8, 0), (8, 45)),
@@ -25,16 +32,29 @@ timeTable = (
 
 
 def genClassSchedule(classList):
+    """
+        解析课程信息，迭代器
+    """
+
+    # 2013年第一学期第一周日期
     _termdate = datetime(2013, 9, 2)
-    ics = Syllics()
+
+    # 遍历课程列表
     for cs in classList:
-        course_num = cs[u"课程号"]
+        course_num  = cs[u"课程号"]
         course_name = cs[u"课程名称"]
-        teacher = cs[u"上课教师"]
-        weeks = []
+        teacher     = cs[u"上课教师"]
+
+        # 获得星期几
         dayofweek = int(cs[u"上课星期"].split(u"星期")[1])
+
+        # 获得上课节次
         m = re.compile(r".(\d{1,2})\-(\d{1,2}).+").match(cs[u"上课节次"])
         beginclass, endclass = [int(i) for i in m.groups()]
+
+        # 获得上课周次
+        # weeks用来存储在哪几个周上课
+        weeks = []
         for i in cs[u"上课周次"].split(','):
             m = re.compile(r"(\d+)\-(\d+).\(?(.)?\)?").match(i)
             beginweek, endweek, numbers = m.groups()
@@ -47,19 +67,24 @@ def genClassSchedule(classList):
 
         place = cs[u"上课地点"]
 
+        # 生成课程详细上课信息的迭代器
         for week in weeks:
             uid = "%s|%s|%d|%d" % (course_num, course_name, week, dayofweek)
+
             starttime = timedelta(weeks=week-1, days=dayofweek-1, 
                 hours=timeTable[beginclass][0][0],
                 minutes=timeTable[beginclass][0][1]) + _termdate
+
             endtime = timedelta(weeks=week-1, days=dayofweek-1, 
                 hours=timeTable[endclass][1][0],
                 minutes=timeTable[endclass][1][1]) + _termdate
-            ics.addEvent(course_name, starttime, endtime, place, 30, teacher, uid)
-    ics.save("b.ics")
-    return ics.ical()
+
+            yield course_name, starttime, endtime, place, teacher, uid
 
 class Syllics():
+    """
+        Syllics类，可根据课程上课信息生成ics文件
+    """
     def __init__(self):
         self.cal = icalendar.Calendar()
         self.cal.add('PRODID', '-//nasta//SYLLABUS//CN')
@@ -67,8 +92,15 @@ class Syllics():
         self.cal.add('X-WR-CALNAME', '课程表')
         self.cal.add('CALSCALE', 'GREGORIAN')
 
-    def addEvent(self, subject, starttime, endtime, place, beforealert, desc, uid):
+    def addEvent(self, subject, starttime, endtime, place, uid, desc="", beforealert=0):
+        """
+            addEvent(self, subject, starttime, endtime, place, uid, desc="", beforealert=0):
+
+        """
+
+        # 添加Event
         event = icalendar.Event()
+
         event.add('CREATED', datetime.now())
         event.add('LAST-MODIFIED', datetime.now())
         event.add('DESCRIPTION', desc)
@@ -79,11 +111,14 @@ class Syllics():
         event.add('PRIORITY', '5')
         event.add('SUMMARY', subject)
         event.add('UID', uid)
+
+        # 添加Alarm
         alarm = icalendar.Alarm()
         alarm.add('TRIGGER', timedelta(seconds=-60*beforealert))
         alarm.add('ACTION', 'DISPLAY')
         alarm.add('DESCRIPTION', 'Reminder')
         event.add_component(alarm)
+
         self.cal.add_component(event)
 
     def save(self, name):
@@ -92,13 +127,9 @@ class Syllics():
         f.close()
 
     def ical(self):
+        """
+        """
         return self.cal.to_ical()
-
-def test():
-    ics = Syllics('test')
-    for i in range(1000):
-        ics.addEvent('subject2', datetime(2013, 8, 23, 17, 14, 0), 'place2', 30, 'desc', i)
-    ics.ical()
 
 if __name__ == '__main__':
     """
--- ical/syllabus.py
+++ ical/syllabus.py
@@ -7,7 +7,16 @@ import BeautifulSoup, re
 import traceback
 import loginfo
 
+_INDEX_URL   = "http://my.tjut.edu.cn/index.portal"
+_POST_URL    = "http://my.tjut.edu.cn/userPasswordValidate.portal"
+_REQUEST_URL = "http://my.tjut.edu.cn/pnull.portal"
+_LOGOUT_URL  = "http://my.tjut.edu.cn/logout.portal"
+
 def getClassList(content):
+    """
+        根据table生成课程列表
+    """
+
     soup   = BeautifulSoup.BeautifulSoup(content)
     names  = []
     result = []
@@ -31,11 +40,9 @@ def getClassList(content):
         count += 1
     return result
 
+
 def getClassTable(username, password):
-    _INDEX_URL   = "http://my.tjut.edu.cn/index.portal"
-    _POST_URL    = "http://my.tjut.edu.cn/userPasswordValidate.portal"
-    _REQUEST_URL = "http://my.tjut.edu.cn/pnull.portal"
-    _LOGOUT_URL  = "http://my.tjut.edu.cn/logout.portal"
+
     param = {
         "Login.Token1" : username,
         "Login.Token2" : password,
@@ -43,34 +50,49 @@ def getClassTable(username, password):
         "gotoOnFail" : "http://my.tjut.edu.cn/loginFailure.portal"
     }
 
-    s = requests.Session()
-    r = s.get(_INDEX_URL)
-    if r.content.find("loginURL:'userPasswordValidate.portal'") == -1:
+    # 带状态的requests对象
+    session = requests.Session()
+
+    # 判断获取首页内容是否正确
+    req = session.get(_INDEX_URL)
+    if req.content.find("loginURL:'userPasswordValidate.portal'") == -1:
         from logerr import logerr
-        logerr(u"获得首页", u"不符合预期", r.content)
+        logerr(u"获得首页", u"不符合预期", req.content)
         return None
 
-    r = s.post(_POST_URL, data=param)
-    if r.content.find("false") != -1:
+    # 登陆
+    req = session.post(_POST_URL, data=param)
+    if req.content.find("false") != -1:
         from logerr import logerr
-        logerr(u"登陆", u"登录失败", r.content)
+        logerr(u"登陆", u"登录失败", req.content)
         return False
 
-    r = s.get(_INDEX_URL)
-    content = r.content
+    # 获取课程表URL
+    req = session.get(_INDEX_URL)
+    content = req.content
     soup = BeautifulSoup.BeautifulSoup(content)
     urls = soup.findAll(attrs={'class':re.compile('^lazyLoadUrl$')})
+
+    # TODO
+    # 不知道什么原因，urls有时只含有1个URL
     try:
         info_url  = _REQUEST_URL + urls[0].attrMap['url']
         class_url = _REQUEST_URL + urls[1].attrMap['url']
+
     except IndexError, e:
+        # fallback url
         class_url = _REQUEST_URL + "?.p=Znxjb20ud2lzY29tLnBvcnRhbC5zaXRlLnYyLmltcGwuRnJhZ21lbnRXaW5kb3d8ZjcxMHx2aWV3fG5vcm1hbHw_&.nctp=true&.ll=true"
 
-    content = s.get(class_url).content
+    # 获取课程表
+    content = session.get(class_url).content
     soup = BeautifulSoup.BeautifulSoup(content)
+
+    # 提取列表
     table = soup.findAll(attrs={"style":"padding-top:0px;"})
     table = table[1].table
-    s.get(_LOGOUT_URL)
+
+    session.get(_LOGOUT_URL)
+
     return str(table)
 
 if __name__ == '__main__':
